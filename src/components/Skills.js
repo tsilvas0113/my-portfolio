d79893abfbca9154ce76edc03d19b37f55900232
Tristan <tsilvas0113@gmail.com>
2022-06-27 15:15:58 -0500

    Add more skills to 'skills' section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,19 +6,30 @@ function Skills () {
             <h2 className="section__title section__title--skills">What I Know</h2>
             <div className="skills">
                 <div className="skill">
-                    <h3>blah</h3>
+                    <h3>HTML</h3>
                     <p></p>
                 </div>
 
                 <div className="skill">
-                    <h3>blah</h3>
+                    <h3>CSS</h3>
                     <p></p>
                 </div>
 
                 <div className="skill">
-                    <h3>blah</h3>
+                    <h3>JavaScript</h3>
                     <p></p>
                 </div>
+
+                <div className="skill">
+                    <h3>React</h3>
+                    <p></p>
+                </div>
+
+                <div className="skill">
+                    <h3>Git</h3>
+                    <p></p>
+                </div>
+
             </div>
 
             <a href="#projects" className="btn">See my projects</a>
