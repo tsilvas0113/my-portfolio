773d8bf249aa9a2f8bf2f18f5ec0112c0d41d372
Tristan <tsilvas0113@gmail.com>
2022-06-28 15:57:09 -0500

    Style the titles and subtitles

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -4,11 +4,11 @@ function Aboutme () {
     return (
         <section className="about-me" id="about">
             <h2 className="section__title section__title--about">Who I am</h2>
-            <p className="section__subtitle section__subtitle--about"></p>
+            <p className="section__subtitle section__subtitle--about">Aspiring web developer from Texas</p>
 
             <div className="about-me__body">
-                <p></p>
-                <p></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
 
             <img src="./images/113.png" alt='Tristan graduation pose' />
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,27 +7,27 @@ function Skills () {
             <div className="skills">
                 <div className="skill">
                     <h3>HTML</h3>
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
 
                 <div className="skill">
                     <h3>CSS</h3>
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
 
                 <div className="skill">
                     <h3>JavaScript</h3>
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
 
                 <div className="skill">
                     <h3>React</h3>
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
 
                 <div className="skill">
                     <h3>Git</h3>
-                    <p></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
 
             </div>
