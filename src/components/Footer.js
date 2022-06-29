bb46b8788b3537d6b7997ff8e5a78318b1883cc1
Tristan <tsilvas0113@gmail.com>
2022-06-29 17:18:53 -0500

    Add footer icons with Font Awesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function Footer () {
     return (
@@ -6,13 +9,13 @@ function Footer () {
             <div className="footer__text">© 2022, Created by Tristan Silvas</div>
             <ul className="social-list">
                 <li className="social-list__item"><a className="social-list__link"
-                href="https://www.linkedin.com/in/tristan-silvas-004273241/">a</a></li>
+                href="https://www.linkedin.com/in/tristan-silvas-004273241/"><FontAwesomeIcon icon={faLinkedin} /></a></li>
                 <li className="social-list__item"><a className="social-list__link"
-                href="https://twitter.com/Chiknnuggets99">b</a></li>
+                href="https://twitter.com/Chiknnuggets99"><FontAwesomeIcon icon={faTwitter} /></a></li>
                 <li className="social-list__item"><a className="social-list__link"
-                href="mailto:tsilvas0113@gmail.com">c</a></li>
+                href="mailto:tsilvas0113@gmail.com"><FontAwesomeIcon icon={faEnvelope} /></a></li>
                 <li className="social-list__item"><a className="social-list__link"
-                href="https://github.com/tsilvas0113">d</a></li>
+                href="https://github.com/tsilvas0113"><FontAwesomeIcon icon={faGithub} /></a></li>
             </ul>
         </footer>
     )
