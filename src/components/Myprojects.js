1b78882fe266a554ff417ec534d3c09451a15c3f
Tristan <tsilvas0113@gmail.com>
2022-06-29 16:20:27 -0500

    Style the 'projects' section

--- src/components/Myprojects.js
+++ src/components/Myprojects.js
@@ -3,8 +3,8 @@ import React from "react";
 function Myprojects () {
     return (
         <section className="projects" id="projects">
-            <h2 className="section__title">My projects</h2>
-            <p className="section__subtitle"></p>
+            <h2 className="section__title section__title--projects">My projects</h2>
+            <p className="section__subtitle section__subtitle--projects">A selection of my range of projects</p>
 
             <div className="portfolio">
                 <a href="#blank" className="portfolio__item">
