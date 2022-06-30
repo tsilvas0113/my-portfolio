6161b24053abe38b06f2eca506a560e0e973f6cd
Tristan <tsilvas0113@gmail.com>
2022-06-30 16:15:40 -0500

    Add click events to nav in 'header' section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,26 @@
 import React from "react";
 
 function Header () {
+    function toggle () {
+        document.body.classList.toggle('nav-open')
+    }
+    function handleClick() {
+        document.body.classList.remove('nav-open')
+    }
     return (
         <header className="header">
             <div className="logo">
                 <div>&lt; devtristan &gt;</div>
             </div>
-            <button className="nav-toggle" aria-label="toggle navigation">
+            <button className="nav-toggle" aria-label="toggle navigation" onClick={toggle}>
                 <span className="hamburger"></span>
             </button>
             <nav className="nav">
                 <ul className="nav__list">
-                    <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
-                    <li className="nav__item"><a href="#skills" className="nav__link">What I Know</a></li>
-                    <li className="nav__item"><a href="#about" className="nav__link">About Me</a></li>
-                    <li className="nav__item"><a href="#projects" className="nav__link">My Projects</a></li>
+                    <li className="nav__item"><a href="#home" className="nav__link" onClick={handleClick}>Home</a></li>
+                    <li className="nav__item"><a href="#skills" className="nav__link" onClick={handleClick}>What I Know</a></li>
+                    <li className="nav__item"><a href="#about" className="nav__link" onClick={handleClick}>About Me</a></li>
+                    <li className="nav__item"><a href="#projects" className="nav__link" onClick={handleClick}>My Projects</a></li>
                 </ul>
             </nav>
         </header>
