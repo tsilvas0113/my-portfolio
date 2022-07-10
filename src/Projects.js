730841b79f4068899d29f1d3540510b29ec75a5a
Tristan <tsilvas0113@gmail.com>
2022-07-10 14:23:22 -0500

    Add another project to 'projcets' section

--- src/Projects.js
+++ src/Projects.js
@@ -17,11 +17,11 @@ const projects = [
     },
     {
         id: 3,
-        projectImg: './images/project1.png',
-        projectTitle: 'Rock/Paper/Scissors Game',
-        projectDescription: 'For this project, I created a rock/paper/scissors game using vanilla JavaScript. Arrow functions, eventlisteners, queryselectors, and callback functions were utilized to add functionality and interaction to the project.',
-        demoLink: 'https://tsilvas0113.github.io/rock-paper-scissors/',
-        githubLink: 'https://github.com/tsilvas0113/rock-paper-scissors'
+        projectImg: './images/project3.png',
+        projectTitle: 'Meme Generator',
+        projectDescription: "I created this project as part of the Learn React for free module from Scrimba. React hooks such as useState to track function component's state and useEffect for API calls, forms, and eventlisteners were used for this project. I also added responsive design for different screen sizes.",
+        demoLink: 'https://tsilvas0113.github.io/meme-generator/',
+        githubLink: 'https://github.com/tsilvas0113/meme-generator'
     },
     {
         id: 4,
