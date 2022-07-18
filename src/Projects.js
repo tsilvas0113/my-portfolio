caf0f11a7cff179cd62555c76d29604da0b667d7
Tristan <tsilvas0113@gmail.com>
2022-07-18 13:54:23 -0500

    Update and add projects in 'project' section

--- src/Projects.js
+++ src/Projects.js
@@ -2,8 +2,8 @@ const projects = [
     {
         id: 1,
         projectImg: './images/project1.png',
-        projectTitle: 'Rock/Paper/Scissors Game',
-        projectDescription: 'For this project, I created a rock/paper/scissors game using vanilla JavaScript. Arrow functions, eventlisteners, queryselectors, and callback functions were utilized to add functionality and interaction to the project.',
+        projectTitle: 'Portfolio Website',
+        projectDescription: 'This is my portfolio website that I created to showcase some of the different skills I know and projects that I have worked on. I created it with React using different features such as hooks, props, and components. CSS was also used to provide different and engaging animations.',
         demoLink: 'https://tsilvas0113.github.io/rock-paper-scissors/',
         githubLink: 'https://github.com/tsilvas0113/rock-paper-scissors'
     },
@@ -25,11 +25,11 @@ const projects = [
     },
     {
         id: 4,
-        projectImg: './images/project1.png',
-        projectTitle: 'Rock/Paper/Scissors Game',
-        projectDescription: 'For this project, I created a rock/paper/scissors game using vanilla JavaScript. Arrow functions, eventlisteners, queryselectors, and callback functions were utilized to add functionality and interaction to the project.',
-        demoLink: 'https://tsilvas0113.github.io/rock-paper-scissors/',
-        githubLink: 'https://github.com/tsilvas0113/rock-paper-scissors'
+        projectImg: './images/project4.png',
+        projectTitle: 'Tenzies',
+        projectDescription: "I built this project as part of the Learn React for free module from Scrimba. React hooks, props, and conditional renderings were used for this project. I also added responsive design for different screen sizes, a timer, a roll counter, and a feature to track the user's best time.",
+        demoLink: 'https://tsilvas0113.github.io/tenzies/',
+        githubLink: 'https://github.com/tsilvas0113/tenzies'
     },
     {
         id: 5,
