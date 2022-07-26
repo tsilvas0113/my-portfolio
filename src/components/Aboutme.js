dcb3488c1dd4f83e519297d3219dc7b1d45165ef
Tristan <tsilvas0113@gmail.com>
2022-07-26 10:02:32 -0500

    Update 'about me' section

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -7,8 +7,8 @@ function Aboutme () {
             <p className="section__subtitle section__subtitle--about">Aspiring web developer from Texas</p>
 
             <div className="about-me__body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>I've been a computer gamer since I was a very young child. This sparked my fascination with technology and led to the development of my love for the web, computers, and video games. As a result, I was certain that I would pursue a computer science degree when the time came.</p>
+                <p>I didn't start realizing how much I liked programming until I started studying for my computer science degree. After spending several years in college, I made the decision that my dream job would be as a software engineer. And although I adore all things related to software development, becoming a web developer is where most of my interests lie.</p>
             </div>
 
             <img src="./images/113.png" alt='Tristan graduation pose' className="about-me__img" />
